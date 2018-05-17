bebb24f76fa4fed6fc07808c370fd524aa7baf6c
Sam Coe <coe6@purdue.edu>
2018-05-17 17:03:50 -0400

    Delete button now works

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = {
                 ev.preventDefault()
                 this.handleSubmit(ev)
             })
+
     },
 
     renderListItem(movie) {
@@ -18,6 +19,14 @@ const app = {
         item.classList.remove('template')
         item.dataset.id = movie.id
         item.querySelector('.movieName').textContent = movie.name
+
+        item
+            .querySelector('.button.alert')
+            .addEventListener('click', this.deleteItem.bind(this, movie))
+
+        item
+            .querySelector('.button.fav')
+            .addEventListener('click', this.favItem.bind(this, movie))
 
         return item
     },
@@ -27,6 +36,9 @@ const app = {
 
         const movieItem = {
             name: f.movieName.value,
+            genre: f.genre.value,
+            status: f.watch.value,
+            fav: false,
             id: ++this.max,
         }
 
@@ -37,6 +49,71 @@ const app = {
 
         f.reset()
     },
+
+    favItem(movie, ev) {
+        const listItem = ev.target.closest('.movie')
+        movie.fav = !movie.fav
+
+        if(movie.fav) {
+            listItem.classList.add('fav')
+        } else {
+            listItem.classList.remove('fav')
+        }
+    },
+
+    deleteItem(movie, ev) {
+        const listItem = ev.target.closest('.movie')
+        listItem.remove()
+
+        for(let i = 0; i < this.movies.length; i++) {
+            if(listItem.dataset.id === this.movies[i].id.toString()) {
+                this.movies.splice(i, 1)
+                break
+            }
+        }
+    },
+
+    filterList(ev) {
+        ev.preventDefault()
+
+    const filter = filterForm.filter.value
+    const movies = document.querySelector('#movies')
+
+    while(movies.hasChildNodes()) {
+        movies.removeChild(movies.lastChild)
+    }
+
+    if(filter === 'noFilter') {
+        const list = document.createElement('ul')
+
+        for(var i = 0; i < movieArray.length; i++) {
+            Object.keys(movieArray[i]).map(value => {
+                const item = renderListItem(value, movieArray[i][value])
+                list.appendChild(item)
+                })
+            createDeleteBttn(list)
+        }
+
+        movies.appendChild(list)
+        return
+    } else {
+        const list = document.createElement('ul')
+
+        for(var i = 0; i < movieArray.length; i++) {
+            if(filter === movieArray[i].Genre) {
+                Object.keys(movieArray[i]).map(value => {
+                    const item = renderListItem(value, movieArray[i][value])
+                    list.appendChild(item)
+                })
+
+                createDeleteBttn(list)
+            }
+        }
+
+        movies.appendChild(list)
+    }
+
+    },
 }
 
 app.init({
@@ -44,3 +121,11 @@ app.init({
     listSelector: '#movieList',
     templateSelector: '.movie.template',
 })
+
+
+
+//homework:
+    //make fav button change the style of the list item in some way
+    //add buttons to move the movie up and down the list
+    //make the buttons also change their place in the array
+    //allow users to edit the names of the movies in the list after they are added (span content editable)
